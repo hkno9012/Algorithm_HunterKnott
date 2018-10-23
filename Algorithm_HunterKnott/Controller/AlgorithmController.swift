b7ea62e68ae6419c3574463fbfc69efa19ab10bf
HunterKnott <hunterknott00@gmail.com>
2018-10-23 13:03:17 -0600

    Copied down a bunch of code
    Still need to figure all of it out

--- Algorithm_HunterKnott/Controller/AlgorithmController.swift
+++ Algorithm_HunterKnott/Controller/AlgorithmController.swift
@@ -32,6 +32,25 @@ class AlgorithmController: UIAlgorithmController
         let stepFive : String = "Put AppDelegate, Assets, and Info.plist in a folder together; put Main.storyboard and LaunchScreen in a folder together; put ViewController in a folder; and add a new Swift file, which will be put in its own folder"
         let stepSix : String = "Go to the project screen on the left, and click the choose info.plist button at the top, then choose the info.plist"
         let stepSeven : String = "Save (command + S), Build (command + B), and Run (command + R)"
+        
+        let algorithm = [stepOne, stepTwo, stepThree, stepFour, stepFive, stepSix, stepSeven]//Like an arraylisyt
+        
+        let attributesDictionary = [NSAttributedStringKey.font : algorithmText.font]
+        let fullAttributedString = NSMutableAttributeString(string: title, attributes: attributesDictionary)
+        
+        for step in algorithm
+        {
+            let bullet : String = "🌞"
+            let formattedStep : String = "\n\(bullet) \(step)"
+            let attributedStringStep : NSMutableAttributedString = NSMutableAttributedString(string : formattedStep)
+            let outlineStyle = createOutlineStyle()
+            
+            attributedStringStep.addAttributes([NSAttributedStringKey.paragraphStyle : outlineStyle], range : NSMakeRange(0, attributedStringStep.length))
+            
+            fullAttributedString.append(attributedStringStep)
+        }
+        
+        algorithmText.attributedText = fullAttributedString
     }
     
     private func createOutlineStyle() -> NSParagraphStyle
